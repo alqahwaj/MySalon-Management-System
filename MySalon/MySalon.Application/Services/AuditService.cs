89b391ebc2baad69f15cc99ee3945069a31bd178
Mohammad Alqahwaji <alqahwaji18@gmail.com>
2026-09-02 15:09:01 +0300

    feat: implement Clean Architecture Auditing, Logging, and Rate Limiting

--- MySalon/MySalon.Application/Services/AuditService.cs
+++ MySalon/MySalon.Application/Services/AuditService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MySalon.Application.Interfaces.Repositories;
+using MySalon.Application.Interfaces.Services;
+using MySalon.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,19 @@ using System.Threading.Tasks;
 
 namespace MySalon.Application.Services
 {
-    internal class AuditService
+    public class AuditService : IAuditService
     {
+        private readonly IAuditRepository _auditRepository;
+
+        public AuditService(IAuditRepository auditRepository)
+        {
+            _auditRepository = auditRepository;
+        }
+
+        public async Task LogActionAsync(string userId, string action, string entityName, string entityId)
+        {
+            var audit = new AuditLog(userId, action, entityName, entityId);
+            await _auditRepository.AddAsync(audit);
+        }
     }
 }
